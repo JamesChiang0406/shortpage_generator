e29b85c9b79fb36a2df815e9e6d15628e971236e
James_C <a9356070@gmail.com>
2020-12-05 16:33:49 +0800

    Copy and all functions done.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const Shortpage = require('./models/shortpage')
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/shortpage_generator', { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -21,13 +22,72 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 
+function randomGenerator() {
+  const numbers = '1234567890'
+  const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+  const UpperCaseLetters = lowerCaseLetters.toUpperCase()
+  let collection = numbers + lowerCaseLetters + UpperCaseLetters
+  let urlText = ''
+
+  for (let i = 0; i < 5; i++) {
+    let index = Math.floor(Math.random() * collection.length)
+    urlText += collection[index]
+    collection = collection.slice(0, index) + collection.slice(index + 1, collection.length)
+  }
+
+  return urlText
+}
+
 app.get('/', (req, res) => {
-  res.render('index')
+  return res.render('index')
 })
 
 app.post('/', (req, res) => {
-  console.log(req.body)
+  const originUrl = req.body.originalUrl
 
+  // 尋找資料庫中是否存在輸入的網站
+  Shortpage.find({ originUrl: originUrl })
+    .lean()
+    .then(results => {
+      if (results.length === 0) {
+        let shortUrl = `${req.protocol}://${req.headers.host}/${randomGenerator()}`
+
+        Shortpage.find()
+          .lean()
+          .then(pages => {
+            // 確定資料庫中不存在相同的短網址
+            pages.forEach(page => {
+              if (page.shortUrl === shortUrl) {
+                shortUrl = `${req.protocol}://${req.headers.host}/${randomGenerator()}`
+              }
+            })
+
+            const newPage = new Shortpage({
+              originUrl: originUrl,
+              shortUrl: shortUrl
+            })
+            return newPage.save()
+              .then(() => {
+                Shortpage.find({ originUrl: originUrl })
+                  .lean()
+                  .then(results => res.render('index', { result: results[0] }))
+                  .catch(error => console.log(error))
+              })
+          })
+      } else {
+        res.render('index', { result: results[0] })
+      }
+    })
+    .catch(error => console.log(error))
+})
+
+app.get('/:position', (req, res) => {
+  const shortUrl = `${req.protocol}://${req.headers.host}${req.originalUrl}`
+
+  Shortpage.find({ shortUrl: shortUrl })
+    .lean()
+    .then(result => res.redirect(`${result[0].originUrl}`))
+    .catch(error => console.log(error))
 })
 
 app.listen(port, () => {
--- models/shortpage.js
+++ models/shortpage.js
@@ -3,13 +3,8 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const shorpageSchema = new Schema({
-  originalURL: {
-    type: String,
-    required: true
-  },
-  shortURL: {
-    type: String
-  }
+  originUrl: String,
+  shortUrl: String
 })
 
 module.exports = mongoose.model('Shortpage', shorpageSchema)
